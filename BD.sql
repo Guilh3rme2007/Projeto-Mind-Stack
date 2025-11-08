757562e48dad835353ce3b6dc506039c86035954
Guilherme Araújo <araujo.gpdem@gmail.com>
2025-11-08 11:15:13 -0300

    banco de dados

---
+++ BD.sql
@@ -0,0 +1,22 @@
+create database MindStack_DB;
+use MindStack_DB;
+
+create table user(
+user_id int auto_increment primary key, 
+username varchar(100) not null unique,
+email varchar(100) not null unique,
+password varchar(255) not null,
+created_at timestamp default current_timestamp
+);
+
+DELIMITER//
+create procedure insertUser(
+in f_username varchar(100),
+in f_email varchar(100),
+in f_password varchar(255)
+)
+begin
+insert into
+//
+DELIMITER;
+
